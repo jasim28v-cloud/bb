59f79be4aa4d96253cafa8069bd1edcde5b9ec1c
jasim28v-cloud <jasim28v@gmail.com>
2026-03-29 01:32:22 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,44 +1,23 @@
-
-// تهئية Firebase باستخدام Compat Mode
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database-compat.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage-compat.js";
-
-// بيانات Firebase الجديدة
+// Firebase Configuration - fsex
 const firebaseConfig = {
-  apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
-  authDomain: "tope-e5350.firebaseapp.com",
-  databaseURL: "https://tope-e5350-default-rtdb.firebaseio.com/",
-  projectId: "tope-e5350",
-  storageBucket: "tope-e5350.appspot.com",
-  messagingSenderId: "187788115549",
-  appId: "1:187788115549:web:5012a1053d2ff7dced97b4",
-  measurementId: "G-V1XM95PMQC"
+    apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
+    authDomain: "tope-e5350.firebaseapp.com",
+    databaseURL: "https://tope-e5350-default-rtdb.firebaseio.com/",
+    projectId: "tope-e5350",
+    storageBucket: "tope-e5350.firebasestorage.app",
+    messagingSenderId: "187788115549",
+    appId: "1:187788115549:web:5012a1053d2ff7dced97b4",
+    measurementId: "G-V1XM95PMQC"
 };
 
-// تهئية Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-const storage = getStorage(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
+const storage = firebase.storage();
 
-// بيانات Cloudinary
+// Cloudinary Configuration
 const CLOUD_NAME = 'dnmpmysk6';
 const UPLOAD_PRESET = 'rsxdfdgw';
 
-// تصدير المتغيرات لاستخدامها في ملف `script.js`
-window.auth = auth;
-window.db = db;
-window.storage = storage;
-window.ref = firebase.database.ref;
-window.set = firebase.database.set;
-window.onValue = firebase.database.onValue;
-window.get = firebase.database.get;
-window.remove = firebase.database.remove;
-window.push = firebase.database.push;
-window.update = firebase.database.update;
-window.CLOUD_NAME = CLOUD_NAME;
-window.UPLOAD_PRESET = UPLOAD_PRESET;
-
-console.log('✅ TOPE System Ready');
+console.log('❤️ fsex System Ready | Shadow Mode Activated');
