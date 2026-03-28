b5447e919f6d0f56d2fa71f63979707184c4608d
jasim28v-cloud <jasim28v@gmail.com>
2026-03-29 00:52:23 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
+    authDomain: "tope-e5350.firebaseapp.com",
+    databaseURL: "https://tope-e5350-default-rtdb.firebaseio.com/",
+    projectId: "tope-e5350",
+    storageBucket: "tope-e5350.appspot.com",
+    messagingSenderId: "187788115549",
+    appId: "1:187788115549:web:5012a1053d2ff7dced97b4",
+    measurementId: "G-V1XM95PMQC"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
+const storage = firebase.storage();
+
+// Cloudinary Configuration
+const CLOUD_NAME = 'dnmpmysk6';
+const UPLOAD_PRESET = 'rsxdfdgw';
+
+console.log('✅ TOPE System Ready');
